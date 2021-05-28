3ea0da8aa94c4e4be98dc0e3bbaa570f0a3e743e
waldo237 <waldo23792@gmail.com>
2021-05-28 08:09:37 -0400

    linted the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,17 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Store } from "./store/store";
-import  { BrowserRouter as Router } from 'react-router-dom';
-import {storeRedux } from './store/storeRedux'
-import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
+import { storeRedux } from "./store/storeRedux";
+import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={storeRedux}>
-    <Store>
-      <Router> 
-        <App />
-      </Router>
-    </Store>
+    <Provider store={storeRedux}>
+      <Store>
+        <Router>
+          <App />
+        </Router>
+      </Store>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
