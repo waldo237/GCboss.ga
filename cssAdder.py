8b70067ea7d44e69c5ad16659a6e358c0044a748
waldo237 <waldo23792@gmail.com>
2021-06-03 20:28:30 -0400

    updated css adder

--- cssAdder.py
+++ cssAdder.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 from bs4.element import Stylesheet
-
+import os
 # <link rel="stylesheet" href="/static/css/main.bebd0e94.chunk.css">
 def main():
+    cssfiles = os.listdir('build\static\css')
+  
     html = open("build\index.html").read()
     soup = BeautifulSoup(html, features="html5lib")
-    link = soup.new_tag('link', rel="stylesheet", href="/static/css/main.bebd0e94.chunk.css")
+    link = soup.new_tag('link', rel="stylesheet", href=f"/static/css/{cssfiles[0]}")
     soup.html.head.insert(2, link)
     print('The link attribute was added to fix css')
 
